73aba47c32ce0411d231bd1c6be8daff829189bc
kehuo <hkhuoke@hotmail.com>
2020-01-03 23:47:46 +0800

    update format

--- nlp_seg/common/date_time.py
+++ nlp_seg/common/date_time.py
@@ -10,7 +10,7 @@ def fn_timer(func):
         t0 = time.time()
         ret = func(*args, **kwargs)
         t1 = time.time()
-        debug_log =  global_var['debug_log']
+        debug_log = global_var['debug_log']
         info = '{:s} function took {:.3f} ms'.format(func.__name__, (t1 - t0) * 1000.0)
         if debug_log == 1:
             Logger.service(info, 'info')
